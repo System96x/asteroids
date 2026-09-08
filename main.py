932b1dcf6d8803fc7bb0083837f4d1ebfe307a20
System96x <system96x@pm.me>
2026-09-08 18:51:25 +0200

    Add delta time and limit FPS to 60

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode ((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -14,6 +16,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
